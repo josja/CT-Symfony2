76793a448a8673b0548903605f16f333278588e8
Josja <josja@20hz.nl>
2016-04-11 12:45:10 +0200

    Added tests

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -12,6 +12,13 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /");
+
+        $this->assertContains('Todo', $client->getResponse()->getContent(), true);
+        $this->assertContains('Priority', $client->getResponse()->getContent(), true);
+        $this->assertContains('Created', $client->getResponse()->getContent(), true);
+        $this->assertContains('Due', $client->getResponse()->getContent(), true);
+        $this->assertContains('Actions', $client->getResponse()->getContent(), true);
+        $this->assertContains('New Todo', $client->getResponse()->getContent(), true);
     }
 }
--- src/AppBundle/Tests/Controller/TodoControllerTest.php
+++ src/AppBundle/Tests/Controller/TodoControllerTest.php
@@ -6,50 +6,67 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class TodoControllerTest extends WebTestCase
 {
-    /*
-    public function testCompleteScenario()
+    public function testCompleteUseCase()
     {
         // Create a new client to browse the application
         $client = static::createClient();
 
         // Create a new entry in the database
-        $crawler = $client->request('GET', '/todo/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /todo/");
-        $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
+        $crawler = $client->request('GET', '/');
+
+        // Check general content
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /");
+
+        $content = $client->getResponse()->getContent();
+        $this->assertContains('Todo', $content, true);
+        $this->assertContains('Priority', $content, true);
+        $this->assertContains('Created', $content, true);
+        $this->assertContains('Due', $content, true);
+        $this->assertContains('Actions', $content, true);
+        $this->assertContains('New Todo', $content, true);
+
+        //fwrite(STDERR, print_r($content, TRUE));
+
+        $crawler = $client->click($crawler->selectLink('New Todo')->link());
 
         // Fill in the form and submit it
         $form = $crawler->selectButton('Create')->form(array(
-            'appbundle_todo[field_name]'  => 'Test',
-            // ... other fields to fill
+            'todo[todo]'  => '1234567Test',
         ));
 
         $client->submit($form);
         $crawler = $client->followRedirect();
 
         // Check data in the show view
-        $this->assertGreaterThan(0, $crawler->filter('td:contains("Test")')->count(), 'Missing element td:contains("Test")');
+        $this->assertGreaterThan(0, $crawler->filter('tr:contains("1234567Test")')->count(),
+            'Missing element td:contains("1234567Test")');
 
         // Edit the entity
-        $crawler = $client->click($crawler->selectLink('Edit')->link());
+        $crawler = $client->click($crawler->filter('tr:contains("1234567Test")')->selectLink('Edit')->link());
 
-        $form = $crawler->selectButton('Update')->form(array(
-            'appbundle_todo[field_name]'  => 'Foo',
-            // ... other fields to fill
+        $form = $crawler->selectButton('Store')->form(array(
+            'todo[todo]'  => 'Foo1234567',
         ));
 
         $client->submit($form);
         $crawler = $client->followRedirect();
 
         // Check the element contains an attribute with value equals "Foo"
-        $this->assertGreaterThan(0, $crawler->filter('[value="Foo"]')->count(), 'Missing element [value="Foo"]');
+        $this->assertGreaterThan(0, $crawler->filter('tr:contains("Foo1234567")')->count(),
+            'Missing element [value="Foo1234567"]');
+
+        // Set the entity to Done
+        $client->submit($crawler->filter('tr:contains("Foo1234567")')->selectButton('Done')->form());
+        $crawler = $client->followRedirect();
+        $class = $crawler->filter('tr:contains("Foo1234567")')->attr('class');
+        $this->assertContains('done', $class, true);
 
         // Delete the entity
-        $client->submit($crawler->selectButton('Delete')->form());
+        $client->submit($crawler->filter('tr:contains("Foo1234567")')->selectButton('Delete')->form());
         $crawler = $client->followRedirect();
 
         // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
-    }
+        $this->assertNotRegExp('/Foo1234567/', $client->getResponse()->getContent());
 
-    */
+    }
 }
